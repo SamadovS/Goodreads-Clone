0fdba0a6511b3522a4a52ef00fff80d9e70eabe7
SamadovS <siroj.origin.profile@gmail.com>
2023-07-15 13:33:12 +0900

    add and design homepage

--- books/tests.py
+++ books/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from books.models import Book
+from users.models import CustomUser
 
 
 class BooksTestCase(TestCase):
@@ -51,9 +52,27 @@ class BooksTestCase(TestCase):
         self.assertNotContains(response, book2.title)
 
 
+class BookReviewTestCase(TestCase):
+    def test_add_review(self):
+        book = Book.objects.create(title='Book1', description='Description1', isbn='111222')
+        user = CustomUser.objects.create(
+            username='david', first_name='siroj', last_name='samadov', email='siroj@mail.ru'
+        )
+        user.set_password('parol')
+        user.save()
+        self.client.login(username='david', password='parol')
 
+        response = self.client.post(reverse('books:reviews', kwargs={'id': book.id}),
+                                    data={'stars_given': 3,
+                                          'comment': 'Nice book'})
 
+        book_reviews = book.bookreview_set.all()
 
+        self.assertEqual(book_reviews.count(), 1)
+        self.assertEqual(book_reviews[0].stars_given, 3)
+        self.assertEqual(book_reviews[0].comment, 'Nice book')
+        self.assertEqual(book_reviews[0].book, book)
+        self.assertEqual(book_reviews[0].user, user)
 
 
 
--- goodreads/urls.py
+++ goodreads/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from goodreads.views import landing_page
+from goodreads.views import landing_page, home_page
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),
     path("users/", include("users.urls")),
     path('books/', include('books.urls')),
-
+    path('home/', home_page, name='home_page'),
     path('admin/', admin.site.urls),
 ]
 
